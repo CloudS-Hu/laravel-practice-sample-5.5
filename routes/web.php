5e0432ff4aa7d3954afac0960023c64e113bab85
clouds.hu <clouds.hu@qq.com>
2018-10-09 10:00:15 +0800

    Finish login and logout

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController'); //resource 方法将遵从 RESTful 架构为用户资源生成路由。该方法接收两个参数，第一个参数为资源名称，第二个参数为控制器名称。
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
